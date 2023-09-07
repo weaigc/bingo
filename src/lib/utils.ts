857add6f021856fc2c08033568e1f92317560d09
weaigc <879821485@qq.com>
2023-09-07 20:51:03 +0800

    revert: MUID

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -126,7 +126,6 @@ export function parseUA(ua?: string, default_ua = DEFAULT_UA) {
 export function mockUser(cookies: Partial<{ [key: string]: string }>) {
   const {
     BING_UA = process.env.BING_UA,
-    MUID = process.env.MUID,
     BING_IP = '',
     _U = defaultUID,
   } = cookies
@@ -138,7 +137,7 @@ export function mockUser(cookies: Partial<{ [key: string]: string }>) {
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
     'User-Agent': ua!,
     'x-ms-useragent': 'azsdk-js-api-client-factory/1.0.0-beta.1 core-rest-pipeline/1.10.3 OS/Win32',
-    cookie: `_U=${_U}; MUID=${MUID || ''}`,
+    cookie: `_U=${_U}`,
   }
 }
 

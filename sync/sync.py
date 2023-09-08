ae60767bf2d6bccf8291e585f5e7ffe1e89f945d
weaigc <879821485@qq.com>
2023-09-08 20:45:02 +0800

    build: sync endpoint

--- sync/sync.py
+++ sync/sync.py
@@ -8,18 +8,19 @@ hf_api = HfApi(
 ENDPOINTS = os.environ.get('ENDPOINTS')
 file = 'endpoint.txt'
 endpoints = ENDPOINTS.split('\n')
-endpoint = endpoints[0].strip()
+endpointIndex = '0'
 try:
     with open(file,"r") as f:
         content=f.read().strip()
-        endpoint = endpoints[1] if content == endpoints[0] else endpoints[0]
+        print('content:', content)
+        endpointIndex = '1' if content == '0' else '0'
         f.close()
 except IOError:
     print("File not accessible")
-    with open(file, "w") as wf:
-        wf.write(endpoint)
-        wf.close()
 
-print('new endpoint', endpoint)
-hf_api.add_space_secret('hf4all/bingo2', 'ENDPOINT', endpoint.strip())
+with open(file, "w") as wf:
+    wf.write(endpointIndex)
+    wf.close()
 
+print('new endpoint', endpoints[int(endpointIndex)])
+hf_api.add_space_secret('hf4all/bingo', 'ENDPOINT', endpoints[int(endpointIndex)].strip())

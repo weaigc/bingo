c1fa065d4f30208e6d48d68984d68f8febb8131f
weaigc <879821485@qq.com>
2023-09-08 17:58:05 +0800

    feat: 增加连接数统计

---
+++ server.js
@@ -0,0 +1,42 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = '0.0.0.0'
+const { PORT = 3000 } = process.env
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port: PORT })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = createServer(handleRequest)
+    .once('error', (err) => {
+      console.error('error', err)
+      process.exit(1)
+    })
+    .listen(PORT, () => {
+      console.log(`> Ready on http://${hostname}:${PORT}`)
+    })
+
+  async function handleRequest(req, res) {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true)
+      const { pathname } = parsedUrl
+
+      if (pathname === '/api/counts') {
+        server.getConnections(function(error, count) {
+          res.end(String(count))
+        })
+      } else {
+        await handle(req, res, parsedUrl)
+      }
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  }
+})

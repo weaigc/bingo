812bd53f758e0176cf738ab72b72a041cad7487b
Happy-clo <support@wdsj.one>
2023-09-21 21:27:09 +0800

    Update openai.ts

--- tests/openai.ts
+++ tests/openai.ts
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 
 const openai = new OpenAI({
   apiKey: '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',
-  baseURL: 'https://https://api.openai.com/v1/chat/completions'
+  baseURL: 'https://api.openai.com/v1/chat/completions'
 });
 
 async function start() {
